2fa697ddef5310d6e4ac589ff522a295dcb12cfd
Shalitha <shalithax@gmail.com>
2020-03-24 17:20:29 +0530

    finished implementations.

--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.notifications;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignUp;
+    private EditText txtName;
+    private static final String CHANNEL_ID = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        txtName = findViewById(R.id.txtName);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description =
+                    getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new
+                    NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+
+        }
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(txtName.getText().length() == 0){
+                    Toast.makeText(MainActivity.this,
+                            "Name is Required", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // Create an explicit intent for an Activity in your app
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent =
+                        PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+                NotificationCompat.Builder builder = new
+                        NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("NOTIFICATION")
+                        .setContentText("CLick here to Register")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(MainActivity.this);
+
+                notificationManager.notify(0, builder.build());
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/notifications/RegisterActivity.java
+++ app/src/main/java/com/example/notifications/RegisterActivity.java
@@ -2,13 +2,29 @@ package com.example.notifications;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
